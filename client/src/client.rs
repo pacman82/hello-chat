479a5c5c7bbea200dd7b9c6444e290a93da67e20
Markus Klein <markus.klein@aleph-alpha.com>
2025-07-17 23:13:51 +0200

    refactor: put output_receiver into client

--- client/src/client.rs
+++ client/src/client.rs
@@ -1,26 +1,53 @@
-use futures_util::Sink;
-use tokio::sync::broadcast;
-use tokio_tungstenite::tungstenite::Message;
+use std::fmt::Display;
 
-use crate::input_sender::InputSender;
+use crate::{input_sender::InputSender, output_receiver::OutputReceiver};
+use futures_util::{Sink, StreamExt};
+use tokio::{sync::broadcast, task::JoinHandle};
+use tokio_tungstenite::{
+    connect_async,
+    tungstenite::{Message, client::IntoClientRequest},
+};
 
 pub struct Client {
-    send_input: tokio::task::JoinHandle<()>,
+    send_input: JoinHandle<()>,
+    receive_output: JoinHandle<()>,
 }
 
 impl Client {
-    pub fn new<W>(write: W, shutdown: broadcast::Receiver<()>) -> Self
+    pub async fn from_websocket(
+        request: impl IntoClientRequest + Unpin,
+        shutdown: broadcast::Receiver<()>,
+    ) -> Self {
+        let (ws_stream, _) = connect_async(request).await.expect("Failed to connect");
+        let (write, read) = ws_stream.split();
+
+        Self::new(write, read, shutdown)
+    }
+
+    pub fn new<W, R, E>(write: W, read: R, shutdown: broadcast::Receiver<()>) -> Self
     where
         W: Sink<Message> + Unpin + Send + 'static,
+        R: futures_util::Stream<Item = Result<Message, E>> + Unpin + Send + 'static,
+        E: Display,
     {
         let input_sender = InputSender::new(write, shutdown.resubscribe());
         let send_input = tokio::spawn(async move {
             input_sender.run().await;
         });
-        Self { send_input }
+
+        let output_receiver = OutputReceiver::new(read, shutdown);
+        let receive_output = tokio::spawn(async move {
+            output_receiver.run().await;
+        });
+
+        Self {
+            send_input,
+            receive_output,
+        }
     }
 
     pub async fn wait_for_shutdown(self) {
         let _ = self.send_input.await;
+        let _ = self.receive_output.await;
     }
 }
--- client/src/main.rs
+++ client/src/main.rs
@@ -2,25 +2,19 @@ mod client;
 mod input_sender;
 mod output_receiver;
 
-use futures_util::StreamExt;
 use tokio::{signal::ctrl_c, sync::broadcast};
-use tokio_tungstenite::connect_async;
 
-use crate::{client::Client, output_receiver::OutputReceiver};
+use crate::client::Client;
 
 #[tokio::main]
 async fn main() {
+    // We create a broadcast channel to commuinate that we want to shutodwn
     let (send_shutdown, recv_shutdown) = broadcast::channel::<()>(1);
-    let (ws_stream, _) = connect_async("ws://127.0.0.1:8080")
-        .await
-        .expect("Failed to connect");
 
-    let (write, read) = ws_stream.split();
-
-    let client = Client::new(write, recv_shutdown.resubscribe());
-
-    let output_receiver = OutputReceiver::new(read, recv_shutdown);
-    let receive_output = tokio::spawn(async move { output_receiver.run().await });
+    // This starts the actual client, which connects to the backend and receives and sends messages
+    // we want to continue to do so until we terminate the client. The await is finished once we
+    // start sending and receiving messages.
+    let client = Client::from_websocket("ws://127.0.0.1:8080", recv_shutdown).await;
 
     // Wait for ctrl+c before initiating shutdown
     let _ = ctrl_c().await;
@@ -32,5 +26,4 @@ async fn main() {
     // Let's be nice and not leave any detached threads. This allows our sender and receiver to
     // implement (and execute) logic for a graceful shutdown, if they should wish so.
     client.wait_for_shutdown().await;
-    let _ = receive_output.await;
 }
