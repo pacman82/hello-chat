eeb1d2efe16358e8c9f6da836ca0e7fd24b7af3b
Markus Klein <markus.klein@aleph-alpha.com>
2025-07-17 16:26:35 +0200

    feat: Client code now no longer leaves the receiver thread detached during shutdown

--- client/src/main.rs
+++ client/src/main.rs
@@ -5,29 +5,44 @@ use tokio::{
     io::{AsyncBufReadExt, BufReader, stdin},
     select,
     signal::ctrl_c,
+    sync::broadcast,
 };
 use tokio_tungstenite::{connect_async, tungstenite::Message};
 
 #[tokio::main]
 async fn main() {
+    let (send_shutdown, recv_shutdown) = broadcast::channel::<()>(1);
     let (ws_stream, _) = connect_async("ws://127.0.0.1:8080")
         .await
         .expect("Failed to connect");
 
     let (write, read) = ws_stream.split();
 
+    let mut shutdown_input = recv_shutdown.resubscribe();
     let stdin_actor = InputSender::new(write, async move {
-        let _ = ctrl_c().await;
-        eprintln!("Ctrl+C received")
+        shutdown_input
+            .recv()
+            .await
+            .expect("shutdown sender must outlive receiver");
     });
     let send_input = tokio::spawn(async move {
         stdin_actor.run().await;
     });
 
-    let output_receiver = OutputReceiver::new(read);
-    let _receive_output = tokio::spawn(async move { output_receiver.run().await });
+    let output_receiver = OutputReceiver::new(read, recv_shutdown);
+    let receive_output = tokio::spawn(async move { output_receiver.run().await });
 
+    // Wait for ctrl+c before initiating shutdown
+    let _ = ctrl_c().await;
+    eprintln!("Ctrl+C received");
+    send_shutdown
+        .send(())
+        .expect("We expect there to be two active receivers");
+
+    // Let's be nice and not leave any detached threads. This allows our sender and receiver to
+    // implement (and execute) logic for a graceful shutdown, if they should wish so.
     let _ = send_input.await;
+    let _ = receive_output.await;
 }
 
 // Actor responsible for reading lines from stdin and sending them to the server
@@ -68,11 +83,12 @@ where
 
 struct OutputReceiver<R> {
     read: R,
+    shutdown: broadcast::Receiver<()>,
 }
 
 impl<R> OutputReceiver<R> {
-    pub fn new(read: R) -> Self {
-        OutputReceiver { read }
+    pub fn new(read: R, shutdown: broadcast::Receiver<()>) -> Self {
+        OutputReceiver { read, shutdown }
     }
 
     pub async fn run<E>(mut self)
@@ -81,7 +97,7 @@ impl<R> OutputReceiver<R> {
         E: Display,
     {
         // Read from websocket and print to stdout
-        while let Some(msg) = self.read.next().await {
+        while let Some(msg) = self.next().await {
             match msg {
                 Ok(Message::Text(text)) => {
                     println!("Received: {}", text);
@@ -94,6 +110,16 @@ impl<R> OutputReceiver<R> {
                     break;
                 }
             }
+        }
+    }
+
+    async fn next<E>(&mut self) -> Option<Result<Message, E>>
+    where
+        R: Stream<Item = Result<Message, E>> + Unpin,
+    {
+        select! {
+            _ = self.shutdown.recv() => None,
+            msg = self.read.next() => msg
         }
     }
 }
